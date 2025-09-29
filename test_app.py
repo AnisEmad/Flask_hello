c25e36bcb7667b44e4e2084f6e6addf4ce2400a9
Anis Emad <anisemad123@gmail.com>
2025-09-29 12:13:46 +0300

    changed the message

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     res = client.get("/")
     assert res.status_code == 200
-    assert b"hello, my friends" in res.data
+    assert b"I'm once again trying" in res.data
